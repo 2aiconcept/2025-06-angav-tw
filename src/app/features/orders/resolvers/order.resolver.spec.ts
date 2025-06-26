976266f3d154e88589dfd1ae61d57d0ee37e54e7
2aiconcept <contact@2aiconcept.com>
2025-06-26 10:19:48 +0200

    unit tests for order resolver and orders resolver

--- src/app/features/orders/resolvers/order.resolver.spec.ts
+++ src/app/features/orders/resolvers/order.resolver.spec.ts
@@ -1,17 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 import { ResolveFn } from '@angular/router';
-
 import { orderResolver } from './order.resolver';
+import { OrderFacade } from '@orders/store/order.facade';
 
 describe('orderResolver', () => {
-  const executeResolver: ResolveFn<boolean> = (...resolverParameters) => 
-      TestBed.runInInjectionContext(() => orderResolver(...resolverParameters));
+  let mockOrderFacade: any;
+  const executeResolver: ResolveFn<boolean> = (...resolverParameters) =>
+    TestBed.runInInjectionContext(() => orderResolver(...resolverParameters));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockOrderFacade = { getById: jest.fn() };
+    TestBed.configureTestingModule({
+      providers: [{ provide: OrderFacade, useValue: mockOrderFacade }],
+    });
   });
 
   it('should be created', () => {
     expect(executeResolver).toBeTruthy();
+  });
+
+  it('should call orderFacade.getById with route param id and return true', () => {
+    const route: any = { params: { id: '123' } };
+    const state: any = {};
+    const result = executeResolver(route, state);
+    expect(mockOrderFacade.getById).toHaveBeenCalledWith('123');
+    expect(result).toBe(true);
   });
 });
--- src/app/features/orders/resolvers/orders.resolver.spec.ts
+++ src/app/features/orders/resolvers/orders.resolver.spec.ts
@@ -1,17 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 import { ResolveFn } from '@angular/router';
-
 import { ordersResolver } from './orders.resolver';
+import { OrderFacade } from '@orders/store/order.facade';
 
 describe('ordersResolver', () => {
-  const executeResolver: ResolveFn<boolean> = (...resolverParameters) => 
-      TestBed.runInInjectionContext(() => ordersResolver(...resolverParameters));
+  let mockOrderFacade: any;
+  const executeResolver: ResolveFn<boolean> = (...resolverParameters) =>
+    TestBed.runInInjectionContext(() => ordersResolver(...resolverParameters));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockOrderFacade = { loadOrders: jest.fn() };
+    TestBed.configureTestingModule({
+      providers: [{ provide: OrderFacade, useValue: mockOrderFacade }],
+    });
   });
 
   it('should be created', () => {
     expect(executeResolver).toBeTruthy();
+  });
+
+  it('should call orderFacade.loadOrders and return true', () => {
+    const route: any = {};
+    const state: any = {};
+    const result = executeResolver(route, state);
+    expect(mockOrderFacade.loadOrders).toHaveBeenCalled();
+    expect(result).toBe(true);
   });
 });
